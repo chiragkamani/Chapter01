99e82433e8634dac5315a8e6182a37a841284d39
chiragkamani <chiragkamani@users.noreply.github.com>
2023-12-25 17:27:47 +0530

    initial commit.

---
+++ HelloCS/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, C#!");
+string name = typeof(Program).Namespace ?? "None!";
+System.Console.WriteLine($"Namespace: {name}");
+
+Console.WriteLine("value is {0}",18.8);
+
+
